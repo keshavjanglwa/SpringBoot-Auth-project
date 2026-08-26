7ab412471b91dbe12af7d7d6320ea3b6fba767fc
keshavjanglwa <keshavjanglwa@gmail.com>
2026-08-26 11:45:34 +0530

    First Commit

---
+++ demo/src/test/java/SpringBoot_Auth/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringBoot_Auth.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
